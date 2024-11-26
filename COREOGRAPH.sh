9766de278a0375c5a17715c1e3ac84a62e7961f3
az-airtech <arnoud@airtechnology.io>
2024-11-26 22:39:52 +0700

    Changed COREOGRAPH.sh to install to system install

--- COREOGRAPH.sh
+++ COREOGRAPH.sh
@@ -1,7 +1,6 @@
 # build s2n-tls
 cmake . -Bbuild \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=./s2n-tls-install \
     -DBUILD_SHARED_LIBS=on
 cmake --build build -j $(nproc)
 CTEST_PARALLEL_LEVEL=$(nproc) ctest --test-dir build
